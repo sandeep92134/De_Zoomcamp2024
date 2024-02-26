0a43ae9ff74c286f7de414dba3c5e674778c8898
Sandeep Chauhan <sandeep921348@gmail.com>
2024-02-26 14:39:35 +0000

    final_sync

--- 04-analytics-engineering/Ny_taxi_data/models/core/fact_fhv_trips.sql
+++ 04-analytics-engineering/Ny_taxi_data/models/core/fact_fhv_trips.sql
@@ -1,10 +1,40 @@
-{{ config(materialized='table') }}
+{{ config(materializaed="table") }}
 
 with
-    fhv as (select *, 'fhv' as service_type from {{ ref('fhv_tripdata') }}),
-    dim_zones as (select * from {{ ref('dim_zones') }} where borough != 'Unknown')
-select fhv.*
-from fhv
-inner join dim_zones as pickup_zone on fhv.pulocationid = pickup_zone.locationid
-inner join dim_zones as dropoff_zone on fhv.dolocationid = dropoff_zone.locationid
-where fhv.pulocationid is not null and fhv.dolocationid is not null
+    fhv_data as (
+        select {{ dbt_utils.star(from=ref("fhv_tripdata")) }}
+        from {{ ref("fhv_tripdata") }}
+    ),
+    trips_unioned as (
+        select *
+        from fhv_data
+    ),
+    dim_zones as (
+        select {{ dbt_utils.star(from=ref("dim_zones")) }} 
+        from {{ ref("dim_zones") }}
+        where borough != 'Unknown'
+    ),
+    joined_location as (
+        select
+            trips_unioned.`pickup_location_id`,
+            pickup_zone.borough as pickup_borough,
+            pickup_zone.zone as pickup_zone,
+            trips_unioned.`dropoff_location_id`,
+            dropoff_zone.borough as dropoff_borough,
+            dropoff_zone.zone as dropoff_zone,
+            trips_unioned.`pickup_datetime`,
+            trips_unioned.`dropoff_datetime`
+        from trips_unioned
+        inner join
+            dim_zones as pickup_zone
+            on trips_unioned.pickup_location_id = pickup_zone.locationid
+        inner join
+            dim_zones as dropoff_zone
+            on trips_unioned.dropoff_location_id = dropoff_zone.locationid
+    ),
+    final as (
+        select *
+        from joined_location
+    )
+select *
+from final
